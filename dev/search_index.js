51858e87abd13babff9f9043c98f3720ca24de9b
Documenter.jl <documenter@juliadocs.github.io>
2023-02-12 21:33:12 +0000

    build based on 61f9feb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastSweeping","category":"page"},{"location":"#FastSweeping","page":"Home","title":"FastSweeping","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastSweeping.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastSweeping]","category":"page"}]
+}
