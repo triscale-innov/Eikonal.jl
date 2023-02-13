d4f180bf74fd4cfecf28236fed6fe608eb0137ec
Documenter.jl <documenter@juliadocs.github.io>
2023-02-13 14:27:40 +0000

    build based on a61b59b

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastSweeping","category":"page"},{"location":"#FastSweeping","page":"Home","title":"FastSweeping","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastSweeping.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastSweeping]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Eikonal","category":"page"},{"location":"#Eikonal","page":"Home","title":"Eikonal","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Eikonal.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Eikonal]","category":"page"}]
 }
